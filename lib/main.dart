e119bef0c4b92a8de76d74d022e44a34c5b917f2
Sarah Jabian <142950731+sxrah404@users.noreply.github.com>
2025-09-19 13:04:16 -0500

    basic setup

--- lib/main.dart
+++ lib/main.dart
@@ -12,7 +12,15 @@ class MainApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return const MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: Scaffold(body: Center(child: Text('Hello World!'))),
+      home: Scaffold(
+        backgroundColor: Colors.white,
+        body: Center(
+          child: Text(
+            'Hello World!',
+            style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
+          ),
+        ),
+      ),
     );
   }
 }
